ef098a5ae65bf8871b9b3dd0bace9d17123dcd32
TOMMASO MONTEDORO s329567 <s329567@studenti.polito.it>
2024-12-19 13:48:54 +0100

    start with 2 different modality

--- 12_sample_GLCD_TP/12_sample_GLCD_TP/Source/RIT/IRQ_RIT.c
+++ 12_sample_GLCD_TP/12_sample_GLCD_TP/Source/RIT/IRQ_RIT.c
@@ -24,11 +24,12 @@
 
 extern int direction;
 volatile int press=0;
-
+volatile int press_1=0;
+volatile int press_2=0;
+extern int chase_mode;
+extern int fright_mode;
 void RIT_IRQHandler (void) {
     static int up = 0, down = 0, sx = 0, dx = 0;
-	
-		// Controllo INT0
 	
 		/*************************INT0***************************/
 		if(press !=0){
@@ -47,6 +48,39 @@ void RIT_IRQHandler (void) {
 						LPC_PINCON->PINSEL4    |= (1 << 20);     /* External interrupt 0 pin selection */
 						}
 				} // end INT0
+		/*************************INT1***************************/
+		if(press_1 !=0){
+				press_1++;
+				if((LPC_GPIO2->FIOPIN & (1<<10)) == 0){
+						switch(press){
+								case 2: 
+										break;
+								default:
+										break;
+						}
+				} else {	/* button released */
+						press=0;			
+						NVIC_EnableIRQ(EINT0_IRQn);							 /* disable Button interrupts			*/
+						LPC_PINCON->PINSEL4    |= (1 << 22);     /* External interrupt 0 pin selection */
+						}
+				} // end INT1
+		/*************************INT2***************************/
+		if(press_2 !=0){
+				press_2++;
+				if((LPC_GPIO2->FIOPIN & (1<<10)) == 0){
+						switch(press){
+								case 2:
+										 pause = (pause == 0) ? 1 : 0;  // Se "pause" è 0, lo imposta a 1, altrimenti a 0
+										break;
+								default:
+										break;
+						}
+				} else {	/* button released */
+						press=0;			
+						NVIC_EnableIRQ(EINT0_IRQn);							 /* disable Button interrupts			*/
+						LPC_PINCON->PINSEL4    |= (1 << 24);     /* External interrupt 0 pin selection */
+						}
+				} // end INT2
 		
 		/*********************JOYSTICK**************************/
     // Controllo joystick UP
--- 12_sample_GLCD_TP/12_sample_GLCD_TP/Source/button_EXINT/IRQ_button.c
+++ 12_sample_GLCD_TP/12_sample_GLCD_TP/Source/button_EXINT/IRQ_button.c
@@ -2,6 +2,8 @@
 #include "lpc17xx.h"
 #include "../GLOBALS.h"
 extern int press;
+extern int press_1;
+extern int press_2;
 
 void EINT0_IRQHandler (void)	  	/* INT0														 */
 {		
@@ -15,13 +17,17 @@ void EINT0_IRQHandler (void)	  	/* INT0														 */
 
 void EINT1_IRQHandler (void)	  	/* KEY1														 */
 {
-	
+	press_1=1;
+	NVIC_DisableIRQ(EINT1_IRQn);		/* disable Button interrupts			 */
+	LPC_PINCON->PINSEL4    &= ~(1 << 22);     /* GPIO pin selection */
 	LPC_SC->EXTINT &= (1 << 1);     /* clear pending interrupt         */
 }
 
 void EINT2_IRQHandler (void)	  	/* KEY2														 */
 {
-	
+	press_2=1;
+	NVIC_DisableIRQ(EINT2_IRQn);		/* disable Button interrupts			 */
+	LPC_PINCON->PINSEL4    &= ~(1 << 24);     /* GPIO pin selection */
   LPC_SC->EXTINT &= (1 << 2);     /* clear pending interrupt         */    
 }
 
