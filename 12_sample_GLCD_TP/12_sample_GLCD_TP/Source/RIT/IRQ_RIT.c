3223f4aa94b3c5fadfa619e1c6b8b852bacf0d8e
TOMMASO MONTEDORO s329567 <s329567@studenti.polito.it>
2025-01-04 14:00:10 +0100

    aggiunto main theme all'inizio

--- 12_sample_GLCD_TP/12_sample_GLCD_TP/Source/RIT/IRQ_RIT.c
+++ 12_sample_GLCD_TP/12_sample_GLCD_TP/Source/RIT/IRQ_RIT.c
@@ -37,59 +37,66 @@ extern int flag_music;
 //SHORTENING UNDERTALE: TOO MANY REPETITIONS
 NOTE song[] = 
 {
-   	// 1
-	{d3, time_semicroma},
-	{d3, time_semicroma},
-	{d4, time_croma},
-	{a3, time_croma},
-	{pause, time_semicroma},
-	{a3b, time_semicroma},
-	{pause, time_semicroma},
-	{g3, time_croma},
-	{f3, time_semicroma*2},
-	{d3, time_semicroma},
-	{f3, time_semicroma},
-	{g3, time_semicroma},
-	// 2
-	{c3, time_semicroma},
-	{c3, time_semicroma},
-	{d4, time_croma},
-	{a3, time_croma},
-	{pause, time_semicroma},
-	{a3b, time_semicroma},
-	{pause, time_semicroma},
-	{g3, time_croma},
-	{f3, time_semicroma*2},
-	{d3, time_semicroma},
-	{f3, time_semicroma},
-	{g3, time_semicroma},
-	// 3
-	{c3b, time_semicroma},
-	{c3b, time_semicroma},
-	{d4, time_croma},
-	{a3, time_croma},
-	{pause, time_semicroma},
-	{a3b, time_semicroma},
-	{pause, time_semicroma},
-	{g3, time_croma},
-	{f3, time_semicroma*2},
-	{d3, time_semicroma},
-	{f3, time_semicroma},
-	{g3, time_semicroma},
-	// 4
-	{a2b, time_semicroma},
-	{a2b, time_semicroma},
-	{d4, time_croma},
-	{a3, time_croma},
-	{pause, time_semicroma},
-	{a3b, time_semicroma},
-	{pause, time_semicroma},
-	{g3, time_croma},
-	{f3, time_semicroma*2},
-	{d3, time_semicroma},
-	{f3, time_semicroma},
-	{g3, time_semicroma},
-	// 5
+    // 1
+    {c3, time_semicroma},    // Do
+    {c4, time_semicroma},    // Re
+    {g3, time_semicroma},    // Mi
+    {e3, time_semicroma},    // Sol 
+    {c4, time_croma},    // Sol
+    {e3, time_croma},// Mi
+    {e3b, time_semicroma},    // Do
+    {e4b, time_semicroma},    // Sol basso
+    {b3b, time_semicroma},// La
+    {f3, time_semicroma},// Si
+		{pause, time_semicroma},    
+		{d4, time_croma},    // Do 
+    {a3, time_croma},    // Do
+    {f3, time_croma},// Si
+    // 1
+    {c3, time_semicroma},    // Do
+    {c4, time_semicroma},    // Re
+    {g3, time_semicroma},    // Mi
+    {e3, time_semicroma},    // Sol 
+    {c4, time_croma},    // Sol
+    {e3, time_croma},// Mi
+    {e3b, time_semicroma},    // Do
+    {e4b, time_semicroma},    // Sol basso
+    {b3b, time_semicroma},// La
+    {f3, time_semicroma},// Si
+		{pause, time_semicroma},    
+		{d4, time_croma},    // Do 
+    {a3, time_croma},    // Do
+    {f3, time_croma},// Si
+    // 1
+    {c3, time_semicroma},    // Do
+    {c4, time_semicroma},    // Re
+    {g3, time_semicroma},    // Mi
+    {e3, time_semicroma},    // Sol 
+    {c4, time_croma},    // Sol
+    {e3, time_croma},// Mi
+    {e3b, time_semicroma},    // Do
+    {e4b, time_semicroma},    // Sol basso
+    {b3b, time_semicroma},// La
+    {f3, time_semicroma},// Si
+		{pause, time_semicroma},    
+		{d4, time_semiminima},    // Do 
+    {a3, time_semiminima},    // Do
+    {f3, time_croma},// Si
+		// 4
+		{c3, time_semicroma},    // Do
+    {c4, time_semicroma},    // Re
+    {g3, time_semicroma},    // Mi
+    {e3, time_semicroma},    // Sol 
+    {c4, time_croma},    // Sol
+		{g3, time_croma},
+		{e3, time_croma},
+		{e3b, time_semicroma},
+		{f3, time_semicroma},
+		{f3, time_semicroma},
+		{g3, time_semicroma},
+		{g3, time_semicroma},
+		{a3, time_semicroma},
+		{c4, time_semicroma},
 };
 
 void RIT_IRQHandler (void)
